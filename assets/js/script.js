69316592223591c115abce389dfd0a2c183c9d34
Virginia <virginia.rabello2107@gmail.com>
2021-04-18 17:48:02 -0500

    buttons working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,56 @@
 
 $("#currentDay").append(moment().format("dddd, MMMM Do YYYY"));
 
+var tasks = [];
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+var loadTasks = function() {
+  tasks= JSON.parse(localStorage.getItem("tasks"));
+  if(tasks){
+     for(let i = 0; i < tasks.length; i++){
+         var text = tasks[i].text;
+         var place = tasks[i].place;
+         var placeId = "#" + place;
+         $(placeId).val(text);
+     }
+    }
+     else {
+      var tasks = {};
+      console.log("entrei else");
+     }
+    
+};
 var hoursArray = $(".row .hour");
         for(let i = 0; i < hoursArray.length; i++){
             var hourLine = JSON.stringify(i+9) + ":00";
                  
     $(".row .hour span")[i].append(moment(hourLine, ["HH.mm"]).format("hh:mm A"));
                 }
+
+    $("body > div > table > tbody > tr > td.save > button").click(function() {
+            
+        var prevSib = $(this).parent().siblings()[1];
+        var nefew = $(prevSib).children();
+        var taskText = $(nefew).val();
+        var taskPlace = nefew.attr("id");
+        console.log(taskPlace);
+        
+        if (taskText) {
+            tasks.push({
+            text: taskText,
+            place: taskPlace
+         });
+
+             
+           saveTasks();
+        }
+        
+    });
+    
+
 var auditTask = function(hourEl) {
         
     var hour = $(hourEl)
@@ -29,6 +73,8 @@ var auditTask = function(hourEl) {
         $(hourEl).next().addClass("future");        
     }
     };
+
+   
   
     setInterval(function() {
     $(".row .hour").each(function() {
@@ -36,3 +82,4 @@ var auditTask = function(hourEl) {
     });
   }, 1000);
 
+ loadTasks();
